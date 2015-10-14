b4bc1d641661c0d4c8692508f051738b0cee6254
Sufflavus <tatashat@gmail.com>
2015-10-14 22:04:22 +0300

    * Grid layout in views

---
+++ Source/Daisy.Terminal/ViewModels/ArticleShowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Daisy.Terminal.Models;
+
+
+namespace Daisy.Terminal.ViewModels
+{
+    public class ArticleShowViewModel : WindowViewModelBase
+    {
+        private Article _article;
+
+
+        public ArticleShowViewModel()
+        {
+        }
+
+
+        public Article Article
+        {
+            get { return _article; }
+            set
+            {
+                _article = value;
+                RaisePropertyChangedEvent("Article");
+            }
+        }
+
+        public override string DisplayName
+        {
+            get { return "Article"; }
+            protected set { base.DisplayName = value; }
+        }
+    }
+}
